71fc332aced36dbfb63fdee83539dd6983a4b57b
devkgn88 <devkgn88@gmail.com>
2024-09-16 00:11:50 +0900

    [KimGanam] Print a and b

---
+++ src/com/gn/day01/Solution02.java
@@ -0,0 +1,16 @@
+package com.gn.day01;
+
+import java.util.Scanner;
+
+public class Solution02 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = "+a);
+        System.out.println("b = "+b);
+        
+        sc.close();
+	}
+}
