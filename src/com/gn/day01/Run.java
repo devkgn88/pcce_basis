ab4243075313ae983d4d93d4b1a11c685d6b9348
devkgn88 <devkgn88@gmail.com>
2024-09-16 00:09:14 +0900

    [KimGanam] Print String

--- src/com/gn/day01/Run.java
+++
@@ -1,7 +0,0 @@
-package com.gn.day01;
-
-public class Run {
-	public static void main(String[] args) {
-		
-	}
-}
--- src/com/gn/day01/Solution01.java
+++ src/com/gn/day01/Solution01.java
@@ -1,5 +1,12 @@
 package com.gn.day01;
 
-public class Solution01 {
+import java.util.Scanner;
 
+public class Solution01 {
+	// Given the string str, write code that prints str.
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
 }
