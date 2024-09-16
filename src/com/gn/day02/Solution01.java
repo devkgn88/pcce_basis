abdeab76e430172279bef20fc9c898fb23062dc4
devkgn88 <devkgn88@gmail.com>
2024-09-16 22:37:22 +0900

    [KimGanam] Printing addition expressions

---
+++ src/com/gn/day02/Solution01.java
@@ -0,0 +1,13 @@
+package com.gn.day02;
+
+import java.util.Scanner;
+
+public class Solution01 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a+" + "+b+" = "+(a + b));
+    }
+}
