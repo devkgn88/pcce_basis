708d6f5e1499846c0d023c47ff66a4d8b2f27c0f
devkgn88 <devkgn88@gmail.com>
2024-09-16 22:49:34 +0900

    [KimGanam] Print by pasting a string

---
+++ src/com/gn/day02/Solution02.java
@@ -0,0 +1,12 @@
+package com.gn.day02;
+
+import java.util.Scanner;
+
+public class Solution02 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        System.out.println(a+b);
+    }
+}
