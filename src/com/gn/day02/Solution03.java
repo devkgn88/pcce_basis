cdb59da15559b283d403a8d6485da52ae6cd551e
devkgn88 <devkgn88@gmail.com>
2024-09-16 23:04:56 +0900

    [KimGanam] Twist The String

---
+++ src/com/gn/day02/Solution03.java
@@ -0,0 +1,13 @@
+package com.gn.day02;
+
+import java.util.Scanner;
+
+public class Solution03 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        for(String s : a.split("")) {
+        	System.out.println(s);
+        }
+    }
+}
