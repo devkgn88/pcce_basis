f8fbdd885fc455922f66a5d715425fdcc0154b32
devkgn88 <devkgn88@gmail.com>
2024-09-26 11:06:34 +0900

    [KimGanam] n letters after string

--- src/com/gn/day09/Run.java
+++ src/com/gn/day09/Run.java
@@ -6,9 +6,15 @@ public class Run {
 //		Solution01 s1 = new Solution01();
 //		s1.solution(new String[] {"0123456789","9876543210","9999999999999"}, 50000, 5, 5);
 		
-		Solution02 s2 = new Solution02();
-		s2.solution(new String[] {"progressive", "hamburger", "hammer", "ahocorasick"}, 
-				new int[][] {{0,4},{1,2},{3,5},{7,7}});
+//		Solution02 s2 = new Solution02();
+//		s2.solution(new String[] {"progressive", "hamburger", "hammer", "ahocorasick"}, 
+//				new int[][] {{0,4},{1,2},{3,5},{7,7}});
+		
+		Solution03 s3 = new Solution03();
+		System.out.println("=== 1 ===");
+		s3.solution("ProgrammerS123", 11);
+		System.out.println("=== 2 ===");
+		s3.solution("He110W0r1d", 5);
 	}
 
 }
---
+++ src/com/gn/day09/Solution03.java
@@ -0,0 +1,8 @@
+package com.gn.day09;
+
+public class Solution03 {
+	public String solution(String my_string, int n) {
+        // return my_string.substring(my_string.length()-n,my_string.length());
+		return my_string.substring(my_string.length()-n);
+    }
+}
