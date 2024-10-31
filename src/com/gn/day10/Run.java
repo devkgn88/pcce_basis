c925f87fdbcaba53c0426ea66d299119c5939e84
devkgn88 <devkgn88@gmail.com>
2024-10-31 09:18:27 +0900

    [KimGanam] n letters before string

---
+++ src/com/gn/day10/Run.java
@@ -0,0 +1,10 @@
+package com.gn.day10;
+
+public class Run {
+	public static void main(String[] args) {
+		Solution01 s1 = new Solution01();
+		System.out.println(s1.solution("ProgrammerS123", 11));	// ProgrammerS
+		System.out.println(s1.solution("He110W0r1d", 5));	// He110 
+		
+	}
+}
