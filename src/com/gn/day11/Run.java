3bd92aafddeb51793c84c793f4127820bb3678ec
devkgn88 <devkgn88@gmail.com>
2024-11-08 09:23:50 +0900

    [kgn] Make Array 1

--- src/com/gn/day11/Run.java
+++ src/com/gn/day11/Run.java
@@ -2,7 +2,13 @@ package com.gn.day11;
 
 public class Run {
 	public static void main(String[] args) {
-		Solution01 s1 = new Solution01();
-		s1.solution("Programmers");
+//		Solution01 s1 = new Solution01();
+//		s1.solution("Programmers");
+		
+		Solution02 s2 = new Solution02();
+		System.out.println("========== [3,6,9] ==========");
+		s2.solution(10, 3);
+		System.out.println("========== [5,10,15] ==========");
+		s2.solution(15, 5);
 	}
 }
