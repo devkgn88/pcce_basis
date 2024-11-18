e54af0334b4f294b3482200b8af160263a60dc8d
devkgn88 <devkgn88@gmail.com>
2024-11-18 11:40:19 +0900

    [Ganam] the first negative number

--- src/com/gn/day12/Run.java
+++ src/com/gn/day12/Run.java
@@ -2,10 +2,16 @@ package com.gn.day12;
 
 public class Run {
 	public static void main(String[] args) {
-		Solution01 s1 = new Solution01();
-		System.out.println("========== 2,3,4,5,6 ==========");
-		s1.solution(3, new int[] {1,5,2}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
-		System.out.println("========== 2,4,6 ==========");
-		s1.solution(4, new int[] {1,5,2}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+//		Solution01 s1 = new Solution01();
+//		System.out.println("========== 2,3,4,5,6 ==========");
+//		s1.solution(3, new int[] {1,5,2}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+//		System.out.println("========== 2,4,6 ==========");
+//		s1.solution(4, new int[] {1,5,2}, new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9});
+		
+		Solution02 s2 = new Solution02();
+		System.out.println("========= 5 ==========");
+		s2.solution(new int[] {12, 4, 15, 46, 38, -2, 15});
+		System.out.println("========== -1 ==========");
+		s2.solution(new int[] {13, 22, 53, 24, 15, 6});
 	}
 }
