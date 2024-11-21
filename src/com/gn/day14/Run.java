7c2498359d4ccec3d26b5cfc7dec779ab2e7001e
devkgn88 <devkgn88@gmail.com>
2024-11-21 09:51:44 +0900

    [KimGanam] Odd vs Even

---
+++ src/com/gn/day14/Run.java
@@ -0,0 +1,11 @@
+package com.gn.day14;
+
+public class Run {
+	public static void main(String[] args) {
+		Solution01 s1 = new Solution01();
+		System.out.println("===== 17 =====");
+		s1.solution(new int[] {4, 2, 6, 1, 7, 6});
+		System.out.println("===== 8 =====");
+		s1.solution(new int[] {-1, 2, 5, 6, 3});
+	}
+}
