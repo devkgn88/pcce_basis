fc4ced1c5e3522465a3494f52f0fb72fae19f8d8
devkgn88 <devkgn88@gmail.com>
2024-11-21 09:59:04 +0900

    [KimGanam] 5 peopole each

--- src/com/gn/day14/Run.java
+++ src/com/gn/day14/Run.java
@@ -2,10 +2,14 @@ package com.gn.day14;
 
 public class Run {
 	public static void main(String[] args) {
-		Solution01 s1 = new Solution01();
-		System.out.println("===== 17 =====");
-		s1.solution(new int[] {4, 2, 6, 1, 7, 6});
-		System.out.println("===== 8 =====");
-		s1.solution(new int[] {-1, 2, 5, 6, 3});
+//		Solution01 s1 = new Solution01();
+//		System.out.println("===== 17 =====");
+//		s1.solution(new int[] {4, 2, 6, 1, 7, 6});
+//		System.out.println("===== 8 =====");
+//		s1.solution(new int[] {-1, 2, 5, 6, 3});
+		
+		Solution02 s2 = new Solution02();
+		System.out.println("===== nami,vex =====");
+		s2.solution(new String[] {"nami", "ahri", "jayce", "garen", "ivern", "vex", "jinx"});
 	}
 }
