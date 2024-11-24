aa6ad633df58f0ff408b6cbd8c4161706320edbf
devkgn88 <devkgn88@gmail.com>
2024-11-25 08:32:01 +0900

    [KimGanam] Convert to LowerCase

--- src/com/gn/day16/Run.java
+++ src/com/gn/day16/Run.java
@@ -2,10 +2,16 @@ package com.gn.day16;
 
 public class Run {
 	public static void main(String[] args) {
-		Solution01 s1 = new Solution01();
-		System.out.println("===== ABCDEFG =====");
-		s1.solution("aBcDeFg");
-		System.out.println("===== AAA =====");
-		s1.solution("AAA");
+//		Solution01 s1 = new Solution01();
+//		System.out.println("===== ABCDEFG =====");
+//		s1.solution("aBcDeFg");
+//		System.out.println("===== AAA =====");
+//		s1.solution("AAA");
+		
+		Solution02 s2 = new Solution02();
+		System.out.println("===== abcdefg =====");
+		s2.solution("aBcDeFg");
+		System.out.println("===== aaa =====");
+		s2.solution("aaa");
 	}
 }
