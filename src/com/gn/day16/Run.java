9f6568f4482619447e05e3a95537b1f501595b0e
devkgn88 <devkgn88@gmail.com>
2024-11-25 08:38:36 +0900

    [KimGanam] Convert string case in array

--- src/com/gn/day16/Run.java
+++ src/com/gn/day16/Run.java
@@ -8,10 +8,17 @@ public class Run {
 //		System.out.println("===== AAA =====");
 //		s1.solution("AAA");
 		
-		Solution02 s2 = new Solution02();
-		System.out.println("===== abcdefg =====");
-		s2.solution("aBcDeFg");
-		System.out.println("===== aaa =====");
-		s2.solution("aaa");
+//		Solution02 s2 = new Solution02();
+//		System.out.println("===== abcdefg =====");
+//		s2.solution("aBcDeFg");
+//		System.out.println("===== aaa =====");
+//		s2.solution("aaa");
+		
+		Solution03 s3 = new Solution03();
+		System.out.println("===== \"aaa\",\"BBB\",\"ccc\",\"DDD\" =====");
+		s3.solution(new String[] {"AAA","BBB","CCC","DDD"});
+		System.out.println("===== \"abc\",\"ABC\" =====");
+		s3.solution(new String[] {"aBc","AbC"});
+		
 	}
 }
