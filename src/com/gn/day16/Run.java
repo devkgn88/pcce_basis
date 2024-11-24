d6dfcbb560d22155616a16aa60d36d3355c7ddac
devkgn88 <devkgn88@gmail.com>
2024-11-25 08:30:51 +0900

    [KimGanam] Convert to UpperCase

---
+++ src/com/gn/day16/Run.java
@@ -0,0 +1,11 @@
+package com.gn.day16;
+
+public class Run {
+	public static void main(String[] args) {
+		Solution01 s1 = new Solution01();
+		System.out.println("===== ABCDEFG =====");
+		s1.solution("aBcDeFg");
+		System.out.println("===== AAA =====");
+		s1.solution("AAA");
+	}
+}
