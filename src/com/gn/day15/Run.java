3596f060ccb188bd4214cf203ba77d493f239f9e
devkgn88 <devkgn88@gmail.com>
2024-11-24 22:57:42 +0900

    [KimGanam]Operations based on length

--- src/com/gn/day15/Run.java
+++ src/com/gn/day15/Run.java
@@ -7,9 +7,15 @@ public class Run {
 //		System.out.println("===== 2, 2, 6, 50, 99, 49 =====");
 //		s1.solution(new int[] {1, 2, 3, 100, 99, 98});
 		
-		Solution03 s3 = new Solution03();
-		System.out.println("=====11=====");
-		s3.solution(new int[] {12, 4, 15, 1, 14});
+//		Solution03 s3 = new Solution03();
+//		System.out.println("=====11=====");
+//		s3.solution(new int[] {12, 4, 15, 1, 14});
+		
+		Solution04 s4 = new Solution04();
+		System.out.println("=====51=====");
+		s4.solution(new int[] {3, 4, 5, 2, 5, 4, 6, 7, 3, 7, 2, 2, 1});
+		System.out.println("=====120=====");
+		s4.solution(new int[] {2, 3, 4, 5});
 		
 	}
 
