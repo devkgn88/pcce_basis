cd583ce224ec29f00b986c6b727ccb3bb7f2861e
devkgn88 <devkgn88@gmail.com>
2024-11-24 22:50:25 +0900

    [KimGanam] Make it 1

--- src/com/gn/day15/Run.java
+++ src/com/gn/day15/Run.java
@@ -3,10 +3,13 @@ package com.gn.day15;
 public class Run {
 
 	public static void main(String[] args) {
-		Solution02 s1 = new Solution02();
-		System.out.println("===== 2, 2, 6, 50, 99, 49 =====");
-		s1.solution(new int[] {1, 2, 3, 100, 99, 98});
+//		Solution02 s1 = new Solution02();
+//		System.out.println("===== 2, 2, 6, 50, 99, 49 =====");
+//		s1.solution(new int[] {1, 2, 3, 100, 99, 98});
 		
+		Solution03 s3 = new Solution03();
+		System.out.println("=====11=====");
+		s3.solution(new int[] {12, 4, 15, 1, 14});
 		
 	}
 
